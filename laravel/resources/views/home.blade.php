65d0567726ba3f933cf8f7479218e7fafb3ab6e0
rafaelsuarez <rafaelaugustosc@gmail.com>
2021-03-09 21:47:09 -0500

    Mar 9 2021

--- laravel/resources/views/home.blade.php
+++ laravel/resources/views/home.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.taxes')
 
 @section('content')
 <div class="container">
@@ -15,6 +15,47 @@
                     @endif
 
                     {{ __('You are logged in!') }}
+                    <!-- Contact-->
+                        <section class="page-section" id="contact">
+                            <div class="container">
+                                <div class="text-center">
+                                    <h2 class="section-heading text-uppercase">Contact Us</h2>
+                                    <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                                </div>
+                                <form id="contactForm" name="sentMessage" novalidate="novalidate">
+                                    <div class="row align-items-stretch mb-5">
+                                        <div class="col-md-6">
+                                            <div class="form-group">
+                                                <input class="form-control" id="name" type="text" placeholder="Your Name *" required="required" data-validation-required-message="Please enter your name." />
+                                                <p class="help-block text-danger"></p>
+                                            </div>
+                                            <div class="form-group">
+                                                <input class="form-control" id="email" type="email" placeholder="Your Email *" required="required" data-validation-required-message="Please enter your email address." />
+                                                <p class="help-block text-danger"></p>
+                                            </div>
+                                            <div class="form-group mb-md-0">
+                                                <input class="form-control" id="pwrd" type="password" placeholder="create your password *" required="required" data-validation-required-message="Please enter your phone number." />
+                                                <p class="help-block text-danger"></p>
+                                            </div>
+                                            <div class="form-group mb-md-0">
+                                                <input class="form-control" id="pwrd" type="password" placeholder="confirm your password *" required="required" data-validation-required-message="Please enter your phone number." />
+                                                <p class="help-block text-danger"></p>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div class="form-group form-group-textarea mb-md-0">
+                                                <textarea class="form-control" id="message" placeholder="Your Message * (optional)" ></textarea>
+                                                <p class="help-block text-danger"></p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="text-center">
+                                        <div id="success"></div>
+                                        <button class="btn btn-primary btn-xl text-uppercase" id="sendMessageButton" type="submit">Sign Up</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </section>
 
 
                 </div>
